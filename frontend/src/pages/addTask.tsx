e7bd4a4a9c20fc6170ddb3b70ca48f6a036e036d
Maroofbuilder <maroofahmad0509@gmail.com>
2024-07-20 23:01:19 +0530

    content update

--- frontend/src/pages/addTask.tsx
+++ frontend/src/pages/addTask.tsx
@@ -10,12 +10,22 @@ const AddTask = () => {
         </div>
         <div className="input-part">
             <div className="input-title">
-                <div className="input-task">Title</div>
+                <div className="label-box">Title</div>
                 <input type="text" />
             </div>
             <div className="input-des input-title">
-                <div className="input-description">Description</div>
+                <div className="label-box">Description</div>
                 <textarea name="" id="" placeholder="Enter task description"></textarea>
+            </div>
+        </div>
+        <div className="date-time">
+            <div className="date-part">
+                <div className="label-box">Due date</div>
+                <input type="date" />
+            </div>
+            <div className="time-part date-part">
+                <div className="label-box">Estimate task</div>
+                <input type="time" />
             </div>
         </div>
     </div>
